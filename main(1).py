2c1ee3361b79a5bb6c8030cff81b94bfcf13d614
Jurasio54 <103755947+Jurasio54@users.noreply.github.com>
2022-06-26 20:45:27 -0400

    Update main(1).py

--- main(1).py
+++ main(1).py
@@ -244,7 +244,7 @@ def onmessage(update,bot:ObigramClient):
         tl_admin_user = os.environ.get('tl_admin_user')
 
         #set in debug
-        tl_admin_user = 'obisoftdev'
+        tl_admin_user = 'demian2008'
 
         jdb = JsonDatabase('database')
         jdb.check_create()
@@ -551,7 +551,7 @@ def onmessage(update,bot:ObigramClient):
                 # set in debug
             #    api_id = 7386053
             #    api_hash = '78d1c032f3aa546ff5176d9ff0e7f341'
-            #    bot_token = '5124841893:AAH30p6ljtIzi2oPlaZwBmCfWQ1KelC6KUg'
+            #    bot_token = '5469710454:AAGZ0grsVBDKmp-JLwUZYt7dVL0FUdp0jdk'
 
             #    chat_id = int(update.message.chat.id)
             #    message_id = int(update.message.message_id)
@@ -567,7 +567,7 @@ def main():
     bot_token = os.environ.get('bot_token')
 
     #set in debug
-    bot_token = '5270983050:AAGBgDc2TXVkuWNiF2gATBjDSr1Xjpd3z2Q'
+    bot_token = '5469710454:AAGZ0grsVBDKmp-JLwUZYt7dVL0FUdp0jdk'
 
     bot = ObigramClient(bot_token)
     bot.onMessage(onmessage)
